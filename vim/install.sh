7a1038304762902acfc81cb2d4e8812897c25f4e
cleitondomazak <cleitondomazak@gmail.com>
2018-04-03 08:10:38 -0300

    Fixed path

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl -fLo "$DOTFILES/vim/autoload/plug.vim" --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
 
 if which neovim >/dev/null 2>&1; then
